4e1758a1fd9a89c6b637d3af2903314c0d7b65eb
Umesh Patil <umesh.patil@gmail.com>
2023-06-19 23:26:28 +0200

    Create Ex-1-Doc_spans_tokens.py

---
+++ Ex-1-Doc_spans_tokens.py
@@ -0,0 +1,26 @@
+# Import the English language class and create the nlp object
+from spacy.lang.en import English
+nlp = English()
+
+# Process the text
+doc = nlp("I like tree kangaroos and narwhals.")
+
+# Select the first token
+first_token = doc[0]
+
+# Print the first token's text
+print(first_token.text)
+
+
+# Process the text
+doc = nlp("In 1990, more than 60% of people in East Asia were in extreme poverty. Now less than 4% are.")
+
+# Iterate over the tokens in the doc
+for token in doc:
+    # Check if the token resembles a number
+    if token.like_num:
+        # Get the next token in the document
+        next_token = doc[token.i + 1]
+        # Check if the next token's text equals '%'
+        if next_token.text == '%':
+            print('Percentage found:', token.text)
