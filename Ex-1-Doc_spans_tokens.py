16dad2add6b189a847b27c49831fa09c2817a3eb
Umesh Patil <umesh.patil@gmail.com>
2023-06-19 23:28:53 +0200

    Update Ex-1-Doc_spans_tokens.py

--- Ex-1-Doc_spans_tokens.py
+++ Ex-1-Doc_spans_tokens.py
@@ -1,3 +1,4 @@
+# (a)
 # Import the English language class and create the nlp object
 from spacy.lang.en import English
 nlp = English()
@@ -11,6 +12,9 @@ first_token = doc[0]
 # Print the first token's text
 print(first_token.text)
 
+
+# (b) Lexical attributes: Use spaCy's Doc and Token objects, and lexical attributes to find percentages in a text.
+# Use the like_num token attribute to check whether a token in the doc resembles a number.
 
 # Process the text
 doc = nlp("In 1990, more than 60% of people in East Asia were in extreme poverty. Now less than 4% are.")
